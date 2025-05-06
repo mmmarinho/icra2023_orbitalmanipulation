c4475614cdd4017050e6666f1e16e770df6e292b
Murilo M. Marinho <murilo.marinho@manchester.ac.uk>
2025-05-06 11:03:12 +0100

    [pypi] Adjusting repository for pypi release.

--- src/marinholab/papers/icra2023/orbital_manipulation/orbital_manipulation.py
+++ src/marinholab/papers/icra2023/orbital_manipulation/orbital_manipulation.py
@@ -33,11 +33,12 @@ def get_tip_rcm_distance(t: DQ, line_direction: DQ, radius: float):
     Eq. (11) of Vitreoretinal Surgical Robotic System with Autonomous Orbital
     Manipulation using Vector-Field Inequalities. Koyama, Y.; Marinho, M. M.;
     and Harada, K. In 2023 IEEE International Conference on Robotics
-    and Automation (ICRA), pages 1–7, May 2023
-    :param t: The tooltip positon of this robot as a pure quaternion.
-    :param line_direction: the shaft line direction as calculated in Eq. (9).
-    :param radius: the radius of the eye.
-    :return: the tip and RCM distance of a given robot.
+    and Automation (ICRA), pages 1–7, May 2023.
+
+    :param t: The tooltip position of this robot as a pure quaternion.
+    :param line_direction: The shaft line direction as calculated in Eq. (9).
+    :param radius: The radius of the eye.
+    :return: The tip and RCM distance of a given robot.
     """
     delta = (float(dot(t, line_direction).q[0]) ** 2) - (np.linalg.norm(vec4(t)) ** 2) + (radius ** 2)
     return float(dot(t, line_direction).q[0] + sqrt(delta))
@@ -48,15 +49,17 @@ def get_tip_rcm_distance_jacobian(t, l, Jt, Jl):
     Gets the Jacobian relating the current RCM position of a robot being
     controlled using orbital manipulation and its joint velocities.
     See get_t_om.
+
     Eq. (18) of Vitreoretinal Surgical Robotic System with Autonomous Orbital
     Manipulation using Vector-Field Inequalities. Koyama, Y.; Marinho, M. M.;
     and Harada, K. In 2023 IEEE International Conference on Robotics
-    and Automation (ICRA), pages 1–7, May 2023
-    :param t: the tooltip positon of this robot as a pure quaternion.
-    :param l: the shaft line direction as calculated in Eq. (9).
-    :param Jt: the translation Jacobian of the tooltip.
-    :param Jl: the line Jacobian of the instrument's shaft.
-    :return: the Jacobian relating the current RCM position of a robot being
+    and Automation (ICRA), pages 1–7, May 2023.
+
+    :param t: The tooltip position of this robot as a pure quaternion.
+    :param l: The shaft line direction as calculated in Eq. (9).
+    :param Jt: The translation Jacobian of the tooltip.
+    :param Jl: The line Jacobian of the instrument's shaft.
+    :return: The Jacobian relating the current RCM position of a robot being
     controlled using orbital manipulation and its joint velocities
     """
     Jh2 = vec4(l).T @ Jt + vec4(t) @ Jl  # Eq. (16)
@@ -66,16 +69,18 @@ def get_tip_rcm_distance_jacobian(t, l, Jt, Jl):
 
 def get_t_om(t: DQ, line_direction: DQ, radius: float):
     """
-    Obtain the current RCM position of a robot arm being controlled using
+    Get the current RCM position of a robot arm being controlled using
     orbital manipulation.
+
     Eq. (10) of Vitreoretinal Surgical Robotic System with Autonomous Orbital
     Manipulation using Vector-Field Inequalities. Koyama, Y.; Marinho, M. M.;
     and Harada, K. In 2023 IEEE International Conference on Robotics
-    and Automation (ICRA), pages 1–7, May 2023
-    :param t: the tooltip positon of this robot as a pure quaternion.
-    :param line_direction: the shaft line direction as calculated in Eq. (9).
-    :param radius: the radius of the eye.
-    :return: the current RCM position of a robot arm being controlled using
+    and Automation (ICRA), pages 1–7, May 2023.
+
+    :param t: The tooltip position of this robot as a pure quaternion.
+    :param line_direction: The shaft line direction as calculated in Eq. (9).
+    :param radius: The radius of the eye.
+    :return: The current RCM position of a robot arm being controlled using
     orbital manipulation.
     """
     return t - line_direction * get_tip_rcm_distance(t, line_direction, radius)
@@ -83,17 +88,19 @@ def get_t_om(t: DQ, line_direction: DQ, radius: float):
 
 def get_J_t_om(t: DQ, line_direction: DQ, radius: float, Jt, Jl):
     """
-    Obtain the orbital translation Jacobian of one of the two robotic arms.
+    Get the orbital translation Jacobian of one of the two robotic arms.
+
     Eq. (19) of Vitreoretinal Surgical Robotic System with Autonomous Orbital
     Manipulation using Vector-Field Inequalities. Koyama, Y.; Marinho, M. M.;
     and Harada, K. In 2023 IEEE International Conference on Robotics
-    and Automation (ICRA), pages 1–7, May 2023
-    :param t: The tooltip positon of this robot as a pure quaternion.
-    :param line_direction: the shaft line direction as calculated in Eq. (9).
-    :param radius: the radius of the eye.
-    :param Jt: the translation Jacobian of the tooltip.
-    :param Jl: the line Jacobian of the instrument's shaft.
-    :return: the orbital translation Jacobian of this robot.
+    and Automation (ICRA), pages 1–7, May 2023.
+
+    :param t: The tooltip position of this robot as a pure quaternion.
+    :param line_direction: The shaft line direction as calculated in Eq. (9).
+    :param radius: The radius of the eye.
+    :param Jt: The translation Jacobian of the tooltip.
+    :param Jl: The line Jacobian of the instrument's shaft.
+    :return: The orbital translation Jacobian of this robot.
     """
     tip_rcm_distance = get_tip_rcm_distance(t, line_direction, radius)
     tip_rcm_distance_jacobian = get_tip_rcm_distance_jacobian(t, line_direction, Jt, Jl)
@@ -102,28 +109,31 @@ def get_J_t_om(t: DQ, line_direction: DQ, radius: float, Jt, Jl):
 
 def get_J_om(t_om_1: DQ, t_om_2: DQ, J_t_om_1, J_t_om_2):
     """
-    Obtain the orbital manipulation Jacobian between two robotic arms.
+    Get the orbital manipulation Jacobian between two robotic arms.
+
     Eq. (20) of Vitreoretinal Surgical Robotic System with Autonomous Orbital
     Manipulation using Vector-Field Inequalities. Koyama, Y.; Marinho, M. M.;
     and Harada, K. In 2023 IEEE International Conference on Robotics
-    and Automation (ICRA), pages 1–7, May 2023
-    :param t_om_1: the result of get_t_om for the first arm.
-    :param t_om_2: the result of get_t_om for the second arm.
-    :param J_t_om_1: the result of get_J_t_om for the first arm.
-    :param J_t_om_2: the result of get_J_t_om for the second arm.
+    and Automation (ICRA), pages 1–7, May 2023.
+
+    :param t_om_1: The result of get_t_om for the first arm.
+    :param t_om_2: The result of get_t_om for the second arm.
+    :param J_t_om_1: The result of get_J_t_om for the first arm.
+    :param J_t_om_2: The result of get_J_t_om for the second arm.
     :return: The orbital manipulation Jacobian between arms.
     """
     return vec4(t_om_1 - t_om_2).T @ np.hstack((J_t_om_1, - J_t_om_2))
 
 def get_D_om(t_om_1: DQ, t_om_2: DQ):
     """
-    Obtain the orbital manipulation squared distance between two robotic arms.
+    Get the orbital manipulation squared distance between two robotic arms.
     Eq. (8) of Vitreoretinal Surgical Robotic System with Autonomous Orbital
     Manipulation using Vector-Field Inequalities. Koyama, Y.; Marinho, M. M.;
     and Harada, K. In 2023 IEEE International Conference on Robotics
-    and Automation (ICRA), pages 1–7, May 2023
-    :param t_om_1: the result of get_t_om for the first arm.
-    :param t_om_2: the result of get_t_om for the second arm.
-    :return: the orbital manipulation distance between both arms.
+    and Automation (ICRA), pages 1–7, May 2023.
+
+    :param t_om_1: The result of get_t_om for the first arm.
+    :param t_om_2: The result of get_t_om for the second arm.
+    :return: The orbital manipulation distance between both arms.
     """
     return np.linalg.norm(vec4(t_om_1 - t_om_2)) ** 2
